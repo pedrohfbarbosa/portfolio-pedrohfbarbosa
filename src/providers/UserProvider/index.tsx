4a0c7b26f6a767fab5ecbb306008cec06f740979
Pedro Barbosa <phf.barbosa@hotmail.com>
2022-12-19 10:37:27 -0300

    fix: update api request

--- src/providers/UserProvider/index.tsx
+++ src/providers/UserProvider/index.tsx
@@ -10,48 +10,20 @@ export const UserProvider = ({ children }: IProviderProps) => {
   const [repos, setRepos] = useState<IReposResponse[] | null>(null);
 
   const getUser = async () => {
-    const userJSON = localStorage.getItem("@USER");
-
-    if (userJSON) {
-      const userParse = JSON.parse(userJSON);
-
-      setUser(userParse);
-
-      return;
-    }
-
     try {
       const { data } = await api.get<IUserResponse>("");
 
       setUser(data);
-
-      const responseJSON = JSON.stringify(data);
-
-      localStorage.setItem("@USER", responseJSON);
     } catch (error) {
       console.log(error);
     }
   };
 
   const getRepos = async () => {
-    const reposJSON = localStorage.getItem("@USER:REPOS");
-
-    if (reposJSON) {
-      const reposParse = JSON.parse(reposJSON);
-
-      setRepos(reposParse);
-
-      return;
-    }
-
     try {
       const { data } = await api.get<IReposResponse[]>("/repos");
 
       setRepos(data);
-
-      const responseJSON = JSON.stringify(data);
-
-      localStorage.setItem("@USER:REPOS", responseJSON);
     } catch (error) {
       console.log(error);
     }
